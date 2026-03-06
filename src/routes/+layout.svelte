1c55b29f25a186bcd6bbe3783c0fe41b5ca020ec
Toni <twtiainen@gmail.com>
2026-03-06 14:23:15 +0200

    Create a tauri app

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,4 +6,6 @@
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
+
+<div data-tauri-drag-region class="h-8 bg-pink-500"></div>
 {@render children()}
--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,17 @@ import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
-export default defineConfig({ plugins: [tailwindcss(), sveltekit(), devtoolsJson()] });
+const host = process.env.TAURI_DEV_HOST;
+
+export default defineConfig({
+	plugins: [tailwindcss(), sveltekit(), devtoolsJson()],
+	clearScreen: false,
+	server: {
+		port: 1420,
+		strictPort: true,
+		host: host || false,
+		hmr: host ? { protocol: 'ws', host, port: 1421 } : undefined,
+		watch: { ignored: ['**/src-tauri/**'] }
+	},
+	envPrefix: ['VITE_', 'TAURI_ENV_*']
+});
